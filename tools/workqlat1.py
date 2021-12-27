a67c0eeaf7b45822b47fb75e91753700f60eb0c1
Imran Khan <imran.f.khan@oracle.com>
2021-12-27 11:12:15 +1100

    Getting work duration histogram

--- tools/workqlat1.py
+++ tools/workqlat1.py
@@ -13,22 +13,31 @@ src = """
 #include <uapi/linux/ptrace.h>
 #include <linux/workqueue.h>
 struct key_t {
+	void *work;
 	void *func;
 };
 
 BPF_HASH(counts, work_func_t);
+BPF_HASH(run_time, struct key_t);
+BPF_HASH(wait_time, struct key_t);
+BPF_HISTOGRAM(run_hist);
 
 TRACEPOINT_PROBE(workqueue, workqueue_execute_start) {
     struct work_struct *work = NULL;
     u64 *count = NULL;
+    u64 *tsp = NULL;
     u64 val = 1;
     work_func_t func = NULL;
     struct key_t key = {};
+    u64 time = 0;
     if (args != NULL) {
         work = args->work;
         if (work != NULL) {
 		func = work->func;
 		key.func = func;
+		key.work = work;
+		time = bpf_ktime_get_ns();
+		run_time.update(&key, &time);
 		count = counts.lookup(&func);
 		if (count != NULL) {
 			*count = *count + 1;
@@ -37,13 +46,36 @@ TRACEPOINT_PROBE(workqueue, workqueue_execute_start) {
 			counts.update(&func, &val);	
 		}
 
-		bpf_trace_printk("Hello work %lx \\n", func);
 	}
     }
     
     return 0;
 }
 
+TRACEPOINT_PROBE(workqueue, workqueue_execute_end) {
+    struct work_struct *work = NULL;
+    u64 *tsp = NULL;
+    work_func_t func = NULL;
+    struct key_t key = {};
+    u64 delta = 0;
+    if (args != NULL) {
+	work = args->work;
+	if (work != NULL) {
+		func = work->func;
+		key.func = func;
+		key.work = work;
+		tsp = run_time.lookup(&key);
+		if (tsp != NULL) {
+			delta = bpf_ktime_get_ns() - *tsp;
+			bpf_trace_printk("Hello work %lu delta %lu \\n", *tsp, delta);
+			run_hist.increment(delta / 1000);
+			run_time.delete(&key);
+		}
+	}
+    }
+
+    return 0;
+}
 """
 
 b = BPF(text=src)
@@ -54,14 +86,12 @@ while 1:
     except KeyboardInterrupt:
         exiting = 1
 
-    print("Printing map", len(counts))
+    print("Work handler invocation count")
     for k, v in sorted(counts.items(), key=lambda counts: counts[1].value):
        func = b.ksym(k)
        print(func.decode("utf-8"), v.value)
-    #for k,v in rec.items():
-        #print("work handler: ",k, "count: ", v) 
-
-    counts.clear()
+    print("Run duration histogram")
+    b["run_hist"].print_linear_hist("microsecs")
 
     
     if exiting:
